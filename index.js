e057ddafa9c16af377925ab4eb9f7a1d48c16554
willSchnere <schnerwill@gmail.com>
2024-10-01 16:54:45 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require('express')
 const app = express();
 
+app.use(express.json());
+
+let personList = []
+
 const port = 3000;
 
-app.get('/',(req,res)=>{
-    res.send('Hello world')
+app.get('/visualizar',(req,res)=>{
+    res.send(personList)
+})
+
+app.get('/params/:id',(req,res)=>{
+    const {id} = req.params;
+    console.log(id)
+})
+
+app.post('/cadastro',(req,res)=>{
+    const {name , age}= req.body;
+    personList.push({name,age})
+    res.send(`Usuario identificado nome do usuario ${name}`)
 })
 
 app.listen(port,()=>{
     console.log(`Example app listening on port ${port}`)
 })
+
+console.log("thau")
