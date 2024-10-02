68ab7b3d2ed81021ae1b404741a443288ab26c20
willSchnere <schnerwill@gmail.com>
2024-10-02 15:02:56 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -10,18 +10,33 @@ const port = 3000;
 app.get('/visualizar',(req,res)=>{
     res.send(personList)
 })
-
+/*
 app.get('/params/:id',(req,res)=>{
     const {id} = req.params;
     res.send(id)
 })
+*/
 
 app.post('/cadastro',(req,res)=>{
-    const {name , age}= req.body;
-    personList.push({name,age})
+    const {name,age,}= req.body;
+    personList.push({name,age,})
     res.send(`Usuario identificado nome do usuario ${name}`)
 })
 
 app.listen(port,()=>{
     console.log(`Example app listening on port ${port}`)
+})
+
+app.delete('/delete/:id',(req,res)=>{
+    const {id} = req.params;
+    const index = parseInt(id,1)
+    personList.splice(index,1)
+    res.send(`Usuario deletado `)
+})
+
+app.put('/update/:id',(req,res)=>{
+    const {id} =req.params;
+    const {name,age} = req.body
+    const index =personList.find(index,1)
+    res.send(`Lista atualizada ${personList}`)
 })
